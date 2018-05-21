ff7596d9bf890d73f4f6a7c16627dc0dfd7f8ed4
hubz00 <hubert.zajac@outlook.com>
2018-05-21 12:30:33 +0200

    floating button on maps

--- mobile/src/main/java/com/navigation/vibration/MapsActivity.java
+++ mobile/src/main/java/com/navigation/vibration/MapsActivity.java
@@ -1,8 +1,11 @@
 package com.navigation.vibration;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -37,6 +40,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         placeName = (String) intent.getSerializableExtra("place_name");
 
         mapFragment.getMapAsync(this);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
 
